63fd38352a2d2eab478a16540727c0995ae0cf53
nobyn0312 <nobyn0312@gmail.com>
2023-02-17 17:38:07 +0900

    t e s t

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Data from "./data.json"
+import TestComponent from './TestComponent';
+
+type USERS =typeof Data;
+
 
 const name ="hello";
 let nameChange ="hello"
@@ -105,14 +110,71 @@ const PC2 :PC={
   id:1,OSType:OS.mac,
 };
 
+// Generics 
+interface GEN<T>{
+  item: T;
+}
+// データ型を後で指定できる
+const gen0: GEN<string> ={item:"hello"};
+const gen1: GEN<number> ={item:12};
+
+// デフォルト
+interface GEN1<T =string>{
+  item: T;
+}
+// 既に型をデフォルトで指定してるので、＜＞がなくてもエラーにならない
+const gen3: GEN1 ={item:"hello"}
+
+// 型を選択して指定 extends
+interface GEN2<T extends string | number> {
+  item:T;
+}
+const gen4:GEN2<string> ={item:"hello"};
+const gen5:GEN2<number>={item:5}
+// const gen6:GEN2<boolean>={item:true}
 
 
-function App() {
+// 型の互換性
+const comp1 ="test";
+let comp2: string = comp1;
+
+// 関数の型の互換性
+let funcComp1 =(x:number) =>{}
+let funcComp2 =(x:string)=>{}
+
+// 関数のジェネリックス
+function funcGEN<T>(props: T){
+  return {item:props}
+}
+const gen6 = funcGEN("test");
+const gen7 = funcGEN<string | null>(null);
+
+// 関数 extends
+function funcGEN1<T extends string | null>(props:T){
+  return {value:props}
+}
+const gen8 =funcGEN1("hello");
+
+// propsの使い方
+interface Props {
+  price:number;
+}
+function funcGen3<T extends Props>(props:T){
+  return {value:props.price};
+}
+const gen10 = funcGen3({price:3});
+
+const funcGen4 =<T extends Props>(props:T)=>{
+  return {value:props.price};
+}
+
+
+
+const  App:React.FC =()=> {
   return (
     <div className="App">
       <header className="App-header">
-
-
+        <TestComponent text="hello from App"/>
       </header>
     </div>
   );
